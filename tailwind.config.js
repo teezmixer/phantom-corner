df6a002d305776878fbe6fe31aab89129f6a322f
valerie-png <valerieburdeina@gmail.com>
2026-07-04 18:21:26 +0200

    2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,34 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  corePlugins: { preflight: true },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accentblue: "var(--accentblue)",
+        accentgold: "var(--accentgold)",
+        accentmint: "var(--accentmint)",
+        accentpink: "var(--accentpink)",
+        accentteal: "var(--accentteal)",
+        accentyellow: "var(--accentyellow)",
+        black: "var(--black)",
+        greydark: "var(--greydark)",
+        greylight: "var(--greylight)",
+        greymid: "var(--greymid)",
+        redbright: "var(--redbright)",
+        reddeep: "var(--reddeep)",
+        redmid: "var(--redmid)",
+        white: "var(--white)",
+      },
+      fontFamily: {
+        "body-primary": "var(--body-primary-font-family)",
+        "body-secondary": "var(--body-secondary-font-family)",
+        "display-hero": "var(--display-hero-font-family)",
+        "display-label": "var(--display-label-font-family)",
+        "display-title": "var(--display-title-font-family)",
+        "hud-small": "var(--hud-small-font-family)",
+        "hud-timer": "var(--hud-timer-font-family)",
+      },
+    },
   },
   plugins: [],
-}
+};
